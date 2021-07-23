e7c9cf5784c7dbe49f41bba4162a21608bc2e35e
Anthony Sim <anthonyysim@gmail.com>
2021-07-23 15:12:50 -0700

    backspace compare

--- 844-backspaceCompare.js
+++ 844-backspaceCompare.js
@@ -1,21 +1,16 @@
+function backspaced(str) {
+  let backspacedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "#") {
+      backspacedStr = backspacedStr.slice(0, backspacedStr.length - 1);
+    } else {
+      backspacedStr += str[i];
+    }
+  }
+  return backspacedStr;
+}
+
 var backspaceCompare = function (s, t) {
-  let sString = "";
-  let tString = "";
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "#") {
-      sString = sString.slice(0, sString.length - 1);
-    } else {
-      sString += s[i];
-    }
-  }
-
-  for (let j = 0; j < t.length; j++) {
-    if (t[j] === "#") {
-      tString = tString.slice(0, tString.length - 1);
-    } else {
-      tString += t[j];
-    }
-  }
-  return sString === tString;
+  return backspaced(s) === backspaced(t);
 };
