8c8664c41205f863d84c9ef590972bf8aed8a710
Anthony Sim <anthonyysim@gmail.com>
2021-07-26 17:26:47 -0700

    073 setZeros revised

--- 073-setZeros.js
+++ 073-setZeros.js
@@ -1,25 +1,23 @@
 var setZeroes = function (matrix) {
   let rowLength = matrix.length;
   let colLength = matrix[0].length;
-  let zerosArr = [];
+  let rows = [];
+  let cols = [];
   // find the locations [row, col] of all the zeros
   for (let i = 0; i < rowLength; i++) {
     for (let j = 0; j < colLength; j++) {
       if (matrix[i][j] === 0) {
-        zerosArr.push([i, j]);
+        rows.push(i);
+        cols.push(j);
       }
     }
   }
-  let len = zerosArr.length;
-  // using the zerosArr, change all the rows to zeros.
-  for (let k = 0; k < len; k++) {
-    matrix[zerosArr[k][0]].fill(0);
-  }
-  // using the zerosArr, change all the cols to zeros.
-  for (let l = 0; l < len; l++) {
-    let column = zerosArr[l][1];
-    for (let m = 0; m < rowLength; m++) {
-      matrix[m][column] = 0;
+  // using the zerosArr, change all the rows & cols to zeros.
+  for (let i = 0; i < rowLength; i++) {
+    for (let j = 0; j < colLength; j++) {
+      if (rows.includes(i) || cols.includes(j)) {
+        matrix[i][j] = 0
+      }
     }
   }
   return matrix;
