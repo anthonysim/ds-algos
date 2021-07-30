963483dedbe375885b5ef313dc31d09ab1c4c1a6
Anthony Sim <anthonyysim@gmail.com>
2021-07-29 23:10:01 -0700

    35 searchInsert

---
+++ 035-searchInsert.js
@@ -0,0 +1,26 @@
+function searchInsert(nums, target) {
+  if (nums[0] === target) return 0;
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    let guess = nums[mid];
+
+    if (guess < target) {
+      left = mid + 1;
+    } else if (guess > target) {
+      right = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] <= 0) {
+      return i;
+    }
+  }
+  return nums.length;
+};
