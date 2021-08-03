daa8690f044e1f7f9106cb42f30f25bc750b5574
Anthony Sim <anthonyysim@gmail.com>
2021-08-03 13:01:47 -0700

    47 permutateUnique

---
+++ 047-permutateUnique.js
@@ -0,0 +1,24 @@
+function permuteUnique(nums) {
+  nums = nums.sort((a, b) => a - b);
+  let res = [];
+  const hash = {};
+
+  function dfs(i) {
+    if (i === nums.length) {
+      if (hash[nums]) {
+        return;
+      }
+      hash[nums.slice()] = nums.slice();
+      res.push(nums.slice());
+      return;
+    }
+
+    for (let j = i; j < nums.length; j++) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+      dfs(i + 1);
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+  dfs(0);
+  return res;
+};
