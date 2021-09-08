5b14d0833f8884d292dfc082cd81cacf9769b3f5
Anthony Sim <anthonyysim@gmail.com>
2021-09-07 19:11:16 -0700

    longestMountain

---
+++ general/medium/845-longestMountain.js
@@ -0,0 +1,31 @@
+function longestMountain(arr) {
+  if (arr.length === 0) return 0;
+
+  let peaksArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i] && arr[i + 1] < arr[i]) {
+      peaksArr.push(i);
+    }
+  }
+  let max = -Infinity;
+
+  for (let j = 0; j < peaksArr.length; j++) {
+    let leftIndex = peaksArr[j];
+    let rightIndex = peaksArr[j];
+    let count = 1;
+
+    while (arr[leftIndex - 1] < arr[leftIndex] && leftIndex >= 0) {
+      count += 1;
+      leftIndex -= 1;
+    }
+
+    while (arr[rightIndex + 1] < arr[rightIndex] && rightIndex >= 0) {
+      count += 1;
+      rightIndex += 1;
+    }
+    max = Math.max(max, count);
+  }
+  return max === -Infinity ? 0 : max;
+};
+
+console.log(longestMountain([2, 1, 4, 7, 3, 2, 5]));
