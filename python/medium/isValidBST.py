1eeb3adfc26e754ead510de0e133c45c04b4ac8a
Anthony Sim <anthonyysim@gmail.com>
2021-09-10 19:21:25 -0700

    containsDup py

--- python/medium/isValidBST.py
+++ python/medium/isValidBST.py
@@ -4,6 +4,7 @@ def isValidBST(root):
             return True
         if node.val <= min or node.val >= max:
             return False
+
         return dfs(node.left, min, node.val) and dfs(node.right, node.val, max)
 
     return dfs(root, float('-inf'), float('inf'))
