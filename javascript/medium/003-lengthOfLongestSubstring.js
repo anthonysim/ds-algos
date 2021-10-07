fdd8ef2779948f8f8bc7beda876beef9806550c8
Anthony Sim <anthonyysim@gmail.com>
2021-10-06 22:43:49 -0700

    refactored

--- javascript/medium/003-lengthOfLongestSubstring.js
+++ javascript/medium/003-lengthOfLongestSubstring.js
@@ -2,11 +2,11 @@ function lengthOfLongestSubstring(str) {
   let hash = [];
   let res = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    while (hash.includes(str[i])) {
+  for (let char of str) {
+    while (hash.includes(char)) {
       hash = hash.splice(1)
     }
-    hash.push(str[i]);
+    hash.push(char);
     res = Math.max(res, Object.keys(hash).length);
   }
   return res;
