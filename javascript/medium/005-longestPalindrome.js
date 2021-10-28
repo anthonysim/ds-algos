6b7902397d76495d1a747f1d37b20938d5f56d18
Anthony Sim <anthonyysim@gmail.com>
2021-10-27 19:28:45 -0700

    longestPalindrome

---
+++ javascript/medium/005-longestPalindrome.js
@@ -0,0 +1,32 @@
+function longestPalindrome(str) {
+  let res = "";
+  let len = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let left = i;
+    let right = i;
+
+    while (left >= 0 && right < str.length && str[left] === str[right]) {
+      if (right - left + 1 > len) {
+        res = str.slice(left, right + 1);
+        len = right - left + 1;
+      }
+      left -= 1;
+      right += 1;
+    }
+    left = i;
+    right = i + 1;
+
+    while (left >= 0 && right < str.length && str[left] === str[right]) {
+      if (right - left + 1 > len) {
+        res = str.slice(left, right + 1);
+        len = right - left + 1;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+  return res;
+}
+
+console.log(longestPalindrome("babad")); // bab
