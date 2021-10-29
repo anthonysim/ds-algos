f82afc919e3d5a4f02bda192114bba5bdeddb005
Anthony Sim <anthonyysim@gmail.com>
2021-10-28 19:55:55 -0700

    isAnagram

---
+++ typescript/easy/242-isAnagram.ts
@@ -0,0 +1,8 @@
+function isAnagram(s: string, t: string): boolean {
+  s = s.split('').sort().join('');
+  t = t.split('').sort().join('');
+
+  return s === t;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
