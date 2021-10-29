09272ee3f748ef98705b930b5cde42260a73196c
Anthony Sim <anthonyysim@gmail.com>
2021-10-28 20:26:48 -0700

    refactored groupAnagrams

--- javascript/medium/049-groupAnagrams.js
+++ javascript/medium/049-groupAnagrams.js
@@ -1,16 +1,11 @@
 function groupAnagrams(strs) {
-  let groups = {};
+  const hash = {};
 
   for (let str of strs) {
     const sortedStr = str.split('').sort().join('');
-
-    if (groups[sortedStr]) {
-      groups[sortedStr].push(str);
-    } else {
-      groups[sortedStr] = [str];
-    }
+    hash[sortedStr] ? hash[sortedStr].push(str) : hash[sortedStr] = [str];
   }
-  return Object.values(groups)
+  return Object.values(hash);
 };
 
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
