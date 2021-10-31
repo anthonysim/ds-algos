667b128fc16ea786893a8822fea75312168179ab
Anthony Sim <anthonyysim@gmail.com>
2021-10-31 11:35:38 -0700

    lca ts

--- typescript/easy/572-isSubtree.ts
+++ typescript/easy/572-isSubtree.ts
@@ -1,15 +1,3 @@
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = (val === undefined ? 0 : val)
-    this.left = (left === undefined ? null : left)
-    this.right = (right === undefined ? null : right)
-  }
-}
-
-
 function isSameTree(s: TreeNode | null, t: TreeNode | null): boolean {
   if (s === null || t === null) return s === null && t === null;
   if (s.val === t.val) {
