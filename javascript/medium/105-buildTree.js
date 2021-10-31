9e4fbb14eee2cc842785683fac8da369b25f72d4
Anthony Sim <anthonyysim@gmail.com>
2021-10-31 12:48:13 -0700

    modified buildTree

--- javascript/medium/105-buildTree.js
+++ javascript/medium/105-buildTree.js
@@ -1,14 +1,13 @@
 function buildTree(preorder, inorder) {
-  function tree(pre, in) {
-    if (pre.length === 0) return null;
+  function tree(preorder, inorder) {
+    if (preorder.length === 0) return null;
 
-    const node = new TreeNode(pre[0]);
-    const index = in.indexOf(pre[0]);
+    const node = new TreeNode(preorder[0]);
+    const index = inorder.indexOf(preorder[0]);
 
-    node.left = tree(pre.slice(1, index + 1), in.slice(0, index));
-    node.right = tree(pre.slice(index + 1), in.slice(index + 1));
+    node.left = tree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    node.right = tree(preorder.slice(index + 1), inorder.slice(index + 1));
     return node;
   }
-
   return tree(preorder, inorder);
 };
