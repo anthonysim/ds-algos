a7054af7d50af2980a81bd00d95aebc18318409f
Anthony Sim <anthonyysim@gmail.com>
2021-10-30 18:39:47 -0700

    isSubtree

---
+++ javascript/easy/572-isSubtree.js
@@ -0,0 +1,19 @@
+
+function isSameTree(s, t) {
+  if (s === null || t === null) {
+    return s === null && t === null;
+  }
+  if (s.val === t.val) {
+    return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+  }
+  return false;
+}
+
+function isSubtree(s, t) {
+  if (s === null) return false;
+  if (isSameTree(s, t)) return true;
+
+  return isSubtree(s.left, t) || isSubtree(s.right, t);
+};
+
+
