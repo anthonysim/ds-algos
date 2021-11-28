9a573c59c021d392dd292cad055a377f25f73a85
Anthony Sim <anthonyysim@gmail.com>
2021-11-27 18:20:12 -0800

    refactored coinChange

--- javascript/medium/032-coinChange.js
+++ javascript/medium/032-coinChange.js
@@ -4,14 +4,12 @@ function coinChange(coins, amount) {
 
   for (let coin of coins) {
     for (let i = 0; i < table.length; i++) {
-      if (coin <= i) {
-        let idx = i - coin;
-        let potentialAmt = table[idx] + 1;
-        table[i] = Math.min(potentialAmt, table[i]);
+      if (i >= coin) {
+        table[i] = Math.min(table[i - coin] + 1, table[i]);
       }
     }
   }
-  return table[table.length - 1] === Infinity ? - 1 : table[table.length - 1];
+  return table[table.length - 1] === Infinity ? -1 : table[table.length - 1];
 }
 
 console.log(coinChange([1, 2, 5], 11));
