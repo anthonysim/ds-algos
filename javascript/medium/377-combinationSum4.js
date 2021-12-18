4fe39306b0bb22a153d48af9e3895c6e2f3c836e
Anthony Sim <anthonyysim@gmail.com>
2021-12-17 17:43:17 -0800

    377 combo sum medium js

---
+++ javascript/medium/377-combinationSum4.js
@@ -0,0 +1,14 @@
+var combinationSum4 = function (nums, target) {
+  const dp = Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i <= target; i++) {
+    for (const n of nums) {
+      if (i - n >= 0) {
+        dp[i] += dp[i - n];
+      }
+    }
+  }
+
+  return dp[target];
+};
