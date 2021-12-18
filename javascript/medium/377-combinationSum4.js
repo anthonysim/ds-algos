83f68a44a72c7ff8daa109145c1298796219bb46
Anthony Sim <anthonyysim@gmail.com>
2021-12-17 18:06:01 -0800

    added notes

--- javascript/medium/377-combinationSum4.js
+++ javascript/medium/377-combinationSum4.js
@@ -12,3 +12,19 @@ var combinationSum4 = function (nums, target) {
 
   return dp[target];
 };
+
+// Notes: basically adding backwards
+// example i = 4, n = 1, 2, 3
+// 4 - 1 = 3
+// 4 - 2 = 2
+// 4 - 3 = 1
+
+// 7 = nums[3] + nums[2] + nums[1];
+
+// dp = [1, 0, 0, 0, 0];
+// dp = [1, 1, 0, 0, 0];
+// dp = [1, 1, 2, 0, 0];
+// dp = [1, 1, 2, 4, 0];
+
+// ----------------------
+// dp = [1, 1, 2, 4, 6];
