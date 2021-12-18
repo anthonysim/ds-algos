3cb0ca1ae1ba04f7918bd05303ec63f8b8bf6419
Anthony Sim <anthonyysim@gmail.com>
2021-12-17 21:31:59 -0800

    added notes to numDecodings.js

--- javascript/medium/091-numDecodings.js
+++ javascript/medium/091-numDecodings.js
@@ -9,11 +9,20 @@ var numDecodings = function (s) {
   dp[0] = 1;
 
   for (let i = 0; i <= s.length; i++) {
+    // iterates through the generated numbers above
     for (let code of decoding) {
       if (s.slice(i, i + code.length) === code) {
         dp[i + code.length] += dp[i];
       }
     }
   }
-  return dp[s.length];
+  return dp[dp.length - 1];
 };
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+// [1, 0, 0]
+
+// '12'.slice(0, 0 + 1) === 1 | 1 === 1
+// '12'.slice(0, 0 + 2) === 12 | 12 === 12
+// '12'.slice(1, 1 + 1) === 2 | 2 === 2
+
