96c7a93be6c950f9172f290891501ddfe0e87366
Anthony Sim <anthonyysim@gmail.com>
2022-01-18 14:49:43 -0800

    modified minWindow

--- javascript/hard/76-minWindow.js
+++ javascript/hard/76-minWindow.js
@@ -1,36 +1,34 @@
-var minWindow = function (str, pattern) {
-  let start = 0;
-  let matched = 0;
-  let substrStart = 0;
-  let min = str.length + 1;
-
-  const hash = pattern.split('').reduce((acc, val) => {
-    if (!(acc[val])) {
-      acc[val] = 0;
-    }
+var minWindow = function (str, target) {
+  const hash = target.split('').reduce((acc, val) => {
+    if (!acc[val]) acc[val] = 0;
     acc[val] += 1;
     return acc;
   }, {})
 
-  for (i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (char in hash) hash[char] -= 1;
-    if (hash[char] >= 0) matched += 1;
+  let start = 0;
+  let matched = 0;
+  let min = Infinity;
+  let substrStart = null;
 
-    while (matched === pattern.length) {
-      if (min > i - start + 1) {
+  for (let i = 0; i < str.length; i++) {
+    let rightChar = str[i];
+
+    if (rightChar in hash) hash[rightChar] -= 1;
+    if (hash[rightChar] >= 0) matched += 1;
+
+    while (matched === target.length) {
+      if (i - start + 1 < min) {
         min = i - start + 1;
         substrStart = start;
       }
 
-      const letter = str[start];
+      const leftChar = str[start];
       start += 1;
-      if (letter in hash) {
-        if (hash[letter] === 0) matched -= 1;
-        hash[letter] += 1;
+      if (leftChar in hash) {
+        if (hash[leftChar] === 0) matched -= 1;
+        hash[leftChar] += 1;
       }
     }
   }
-  if (min > str.length) return '';
-  return str.substring(substrStart, substrStart + min);
-}
+  return min === Infinity ? '' : str.slice(substrStart, substrStart + min);
+};
