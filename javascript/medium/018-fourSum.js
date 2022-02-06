d8db74abea964d24c01a431447f8083d13d36943
Anthony Sim <anthonyysim@gmail.com>
2022-02-06 10:31:00 -0800

    fourSum

---
+++ javascript/medium/018-fourSum.js
@@ -0,0 +1,35 @@
+var fourSum = function (nums, target) {
+  nums.sort((a, b) => a - b);
+
+  let res = [];
+
+  for (i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    for (j = i + 1; j < nums.length - 2; j++) {
+
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+        if (sum === target) {
+          res.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          while (left < right && nums[left] === nums[left + 1]) left += 1;
+          while (left < right && nums[right] === nums[right - 1]) right -= 1;
+          left += 1;
+          right -= 1;
+        } else if (sum < target) {
+          left += 1;
+        } else {
+          right -= 1;
+        }
+      }
+    }
+  }
+  return res;
+};
--- javascript/medium/784-letterCasePermutation.js
+++ javascript/medium/784-letterCasePermutation.js
@@ -1,29 +1,29 @@
-function letterCasePermutation(s) {
+function letterCasePermutation(str) {
   let result = [];
   let slate = []
 
-  function dfs(i, s) {
-    if (i === s.length) {
+  function dfs(i, str) {
+    if (i === str.length) {
       result.push(slate.join(''));
       return;
     }
 
-    let char = s[i];
+    let char = str[i];
 
     if (char.match(/[a-zA-Z]/g)) {
       slate.push(char.toUpperCase());
-      dfs(i + 1, s, slate);
+      dfs(i + 1, str);
       slate.pop();
 
       slate.push(char.toLowerCase());
-      dfs(i + 1, s, slate);
+      dfs(i + 1, str);
       slate.pop();
     } else {
       slate.push(char);
-      dfs(i + 1, s, slate);
+      dfs(i + 1, str);
       slate.pop()
     }
   }
-  dfs(0, s);
+  dfs(0, str);
   return result;
 };
