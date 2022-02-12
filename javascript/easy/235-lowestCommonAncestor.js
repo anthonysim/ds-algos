7dee43fccb53e64ca56c0f5afea07404837676fa
Anthony Sim <anthonyysim@gmail.com>
2022-02-12 15:32:11 -0800

    added notes on time and space complexity

--- javascript/easy/235-lowestCommonAncestor.js
+++ javascript/easy/235-lowestCommonAncestor.js
@@ -11,3 +11,7 @@ var lowestCommonAncestor = function (root, p, q) {
     }
   }
 };
+
+// Time Complexity: O(N)O(N), where NN is the number of nodes in the BST. In the worst case we might be visiting all the nodes of the BST.
+
+// Space Complexity: O(N)O(N). This is because the maximum amount of space utilized by the recursion stack would be NN since the height of a skewed BST could be NN.
