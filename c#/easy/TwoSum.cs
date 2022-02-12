5055cd70add5f1cefdcf47af6b785dbdacc9b526
Anthony Sim <anthonyysim@gmail.com>
2022-02-11 21:58:41 -0800

    TwoSum C#

---
+++ c#/easy/TwoSum.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+  public int[] TwoSum(int[] nums, int target)
+  {
+    Dictionary<int, int> hash = new Dictionary<int, int>();
+
+    for (var i = 0; i < nums.Length; i++)
+    {
+      var diff = target - nums[i];
+
+      if (hash.ContainsKey(diff))
+      {
+        return new int[] { hash[diff], i };
+      }
+      else
+      {
+        hash[nums[i]] = i;
+      }
+    }
+    return new int[2];
+  }
+}
