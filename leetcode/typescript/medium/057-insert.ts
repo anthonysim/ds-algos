dba25e17ffa00dc9642d94807f8b3a990ae79cce
Anthony Sim <anthonyysim@gmail.com>
2022-02-15 11:11:13 -0800

    insert intervals ts 57

---
+++ leetcode/typescript/medium/057-insert.ts
@@ -0,0 +1,15 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+  intervals = [...intervals, newInterval].sort((a, b) => a[0] - b[0]);
+  const res: number[][] = [intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+    let prev: number[] = res[res.length - 1];
+
+    if (prev[1] >= intervals[i][0]) {
+      prev[1] = Math.max(prev[1], intervals[i][1]);
+    } else {
+      res.push(intervals[i]);
+    }
+  }
+  return res;
+};
