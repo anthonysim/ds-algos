b29ea044596fd6901101a28bbad5dd9c46b2955a
Anthony Sim <anthonyysim@gmail.com>
2022-02-17 19:20:05 -0800

    reformated

--- leetcode/c#/easy/MaxProfit.cs
+++ leetcode/c#/easy/MaxProfit.cs
@@ -1,12 +1,15 @@
-public class Solution {
-    public int MaxProfit(int[] prices) {
-        int prev = int.MaxValue;
-        int max = int.MinValue;
+public class Solution
+{
+  public int MaxProfit(int[] prices)
+  {
+    int prev = int.MaxValue;
+    int max = int.MinValue;
 
-        foreach (var price in prices) {
-            prev = Math.Min(prev, price);
-            max = Math.Max(max, price - prev);
-        }
-        return max;
+    foreach (var price in prices)
+    {
+      prev = Math.Min(prev, price);
+      max = Math.Max(max, price - prev);
     }
+    return max;
+  }
 }
