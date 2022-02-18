6c7adc1d13efacc7c902c70f8ddc78a94165a28b
Anthony Sim <anthonyysim@gmail.com>
2022-02-17 19:19:08 -0800

    MaxProfit c# 121

---
+++ leetcode/c#/easy/MaxProfit.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int prev = int.MaxValue;
+        int max = int.MinValue;
+
+        foreach (var price in prices) {
+            prev = Math.Min(prev, price);
+            max = Math.Max(max, price - prev);
+        }
+        return max;
+    }
+}
