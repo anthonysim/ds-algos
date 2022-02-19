e1c909bd89a8667a0eff188af50feb99c8779214
Anthony Sim <anthonyysim@gmail.com>
2022-02-19 13:13:03 -0800

    added time and space complexity meeting rooms II

--- leetcode/javascript/medium/222-countNodes.js
+++ leetcode/javascript/medium/222-countNodes.js
@@ -1,17 +1,28 @@
-function countNodes(root) {
-  if (root === null) return 0;
+var minMeetingRooms = function (intervals) {
+  let startTimes = intervals.map(num => num[0]).sort((a, b) => a - b);
+  let endTimes = intervals.map(num => num[1]).sort((a, b) => a - b);;
 
-  let queue = [root];
+  startTimes.sort((a, b) => a - b);
+  endTimes.sort((a, b) => a - b);
+
+  let max = 0;
   let count = 0;
-  while (queue.length) {
-    let len = queue.length;
+  let s = 0;
+  let e = 0;
 
-    for (let i = 0; i < len; i++) {
-      let node = queue.shift();
+  while (s < startTimes.length) {
+    if (startTimes[s] < endTimes[e]) {
+      s += 1;
       count += 1;
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+    } else {
+      e += 1;
+      count -= 1;
     }
+    max = Math.max(max, count);
   }
-  return count;
+  return max;
 };
+
+// Time Complexity: O(NlogN) because all we are doing is sorting the two arrays for start timings and end timings individually and each of them would contain NN elements considering there are NN intervals.
+
+// Space Complexity: O(N) because we create two separate arrays of size NN, one for recording the start times and one for the end times.
