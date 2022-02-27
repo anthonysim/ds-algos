505f739a539d3163b8476187cd537a7a1cd6eb56
Anthony Sim <anthonyysim@gmail.com>
2022-02-27 10:29:28 -0800

    number of islands

---
+++ leetcode/javascript/medium/200-numIslands.js
@@ -0,0 +1,44 @@
+function explore(grid, r, c, visited) {
+  const rowInbounds = 0 <= r && r < grid.length;
+  const colInbounds = 0 <= c && c < grid[0].length;
+
+  if (!rowInbounds || !colInbounds) return false;
+  if (grid[r][c] === '0') return false;
+
+  const pos = r + ',' + c;
+
+  if (visited.has(pos)) return false;
+  visited.add(pos);
+
+  explore(grid, r - 1, c, visited);
+  explore(grid, r + 1, c, visited);
+  explore(grid, r, c - 1, visited);
+  explore(grid, r, c + 1, visited);
+
+  return true;
+}
+
+
+function numIslands(grid) {
+  const visited = new Set();
+  let count = 0;
+
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      if (explore(grid, r, c, visited)) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+
+const grid = [
+  ["1", "1", "0", "0", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "1", "0", "0"],
+  ["0", "0", "0", "1", "1"]
+]
+
+console.log(numIslands(grid)); // 3
