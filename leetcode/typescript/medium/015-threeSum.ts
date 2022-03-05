d75b2f234eb0bb7e0062b3662694c25e725db38c
Anthony Sim <anthonyysim@gmail.com>
2022-03-04 23:10:30 -0800

    3sum ts

---
+++ leetcode/typescript/medium/015-threeSum.ts
@@ -0,0 +1,27 @@
+function threeSum(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let left: number = i + 1;
+    let right: number = nums.length - 1;
+
+    while (left < right) {
+      let total = nums[i] + nums[left] + nums[right];
+
+      if (total === 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        while (nums[left] === nums[left + 1]) left += 1;
+        while (nums[right] === nums[right - 1]) right -= 1;
+        left += 1;
+        right -= 1;
+      } else if (total < 0) {
+        left += 1;
+      } else {
+        right -= 1;
+      }
+    }
+    while (nums[i] === nums[i + 1]) i += 1;
+  }
+  return res;
+};
