c92b40bf40899a08ce2f3cb5044f513a614d3dac
Anthony Sim <anthonyysim@gmail.com>
2022-03-08 17:52:29 -0800

    coin change

--- leetcode/javascript/medium/322-coinChange.js
+++ leetcode/javascript/medium/322-coinChange.js
@@ -1,9 +1,15 @@
-function coinChange(coins, amount) {
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
 
+  for (let coin of coins) {
+    for (let i = 0; i < dp.length; i++) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
+    }
+  }
+  return dp[dp.length - 1] === Infinity ? -1 : dp[dp.length - 1];
 };
 
-console.log(coinChange([1, 2, 5], 11));
-// console.log(coinChange([2], 3));
-// console.log(coinChange([2], 3));
-
-// console.log(coinChange([1], 2));
+console.log(coinChange([1, 2, 5], 11)); // 3
