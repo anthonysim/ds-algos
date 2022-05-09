f659f181e8d89b12ad093f107e07fa140e37bfd3
Anthony Sim <anthonyysim@gmail.com>
2022-05-08 22:36:15 -0700

    213 rob ts

---
+++ leetcode/typescript/medium/213-rob.ts
@@ -0,0 +1,22 @@
+function robber(nums: number[]): number {
+  let prev = 0;
+  let rob1 = 0;
+  let rob2 = 0;
+
+  for (let num of nums) {
+    prev = Math.max(rob1 + num, rob2);
+    rob1 = rob2;
+    rob2 = prev;
+  }
+  return prev;
+}
+
+
+function rob(nums: number[]): number {
+  if (nums.length === 1) return nums[0];
+
+  let first: number[] = nums.slice(0, nums.length - 1);
+  let second: number[] = nums.slice(1);
+
+  return Math.max(robber(first), robber(second));
+};
