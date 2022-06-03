0de9b374a66426a2ac304a0f182ebff04a958f16
Anthony Sim <anthonyysim@gmail.com>
2022-06-02 20:11:04 -0700

    updated isPalindrome

--- leetcode/javascript/easy/125-isPalindrome.js
+++ leetcode/javascript/easy/125-isPalindrome.js
@@ -1,16 +1,21 @@
-function isPalindrome(str) {
-  let arr = str.toLowerCase().match(/[a-zA-Z0-9]/g);
+var isPalindrome = function (s) {
+  let str = s.match(/[a-zA-Z0-9]/gi);
 
-  if (!arr || str.length === 1) return true;
+  if (s === null || str === null) return true;
 
   let left = 0;
-  let right = arr.length - 1;
+  let right = str.length - 1;
+
   while (left < right) {
-    if (arr[left] !== arr[right]) {
+    if (str[left].toLowerCase() !== str[right].toLowerCase()) {
       return false;
     }
     left += 1;
     right -= 1;
   }
+
   return true;
 };
+
+// time: O(n)
+// space: O(1)
