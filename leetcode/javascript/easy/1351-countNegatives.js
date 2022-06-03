365b382af2915fc1c9beac071475a3c641b73531
Anthony Sim <anthonyysim@gmail.com>
2022-06-03 00:46:58 -0700

    updated countNegavtives

--- leetcode/javascript/easy/1351-countNegatives.js
+++ leetcode/javascript/easy/1351-countNegatives.js
@@ -1,12 +1,19 @@
 var countNegatives = function (grid) {
   let count = 0;
+  let i = 0;
+  let j = grid[0].length - 1;
+  let width = grid[0].length - 1;
 
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (grid[i][j] < 0) {
-        count += 1;
-      }
+
+  while (i < grid.length && j >= -1) {
+    if (grid[i][j] >= 0 || j < 0) {
+      count += (width - j);
+      i += 1;
+    } else {
+      j -= 1;
     }
   }
   return count;
-};
+}
+
+// time: O(n + m)
