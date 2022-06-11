2308e6f0df02af0226ecc6d4b9ccbe87da8b097e
Anthony Sim <anthonyysim@gmail.com>
2022-06-10 17:34:15 -0700

    binary search find min rotated

---
+++ algomonster/binarysearch/findMinRotated.js
@@ -0,0 +1,24 @@
+// A sorted array of unique integers was rotated at an unknown pivot. For example, [10, 20, 30, 40, 50] becomes [30, 40, 50, 10, 20]. Find the index of the minimum element in this array. All the numbers are unique.
+
+function findMinRotated(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let boundary = -1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] > arr[right]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+            boundary = mid;
+        }
+    }
+    return boundary;
+}
+
+console.log(findFirstOccurrence([30, 40, 50, 10, 20])); // index at 3
+
+// time: O(logN)
+// space: O(1)
