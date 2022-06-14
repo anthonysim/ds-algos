4a8ffe468eae4719f8e085bfd1c2f82d6f621b7d
Anthony Sim <anthonyysim@gmail.com>
2022-06-13 17:44:00 -0700

    isBalanced js 110

---
+++ leetcode/javascript/easy/110.isBalanced.js
@@ -0,0 +1,15 @@
+function findHeight(node) {
+  if (node == null) return 0;
+
+  return Math.max(findHeight(node.left), findHeight(node.right)) + 1;
+}
+
+
+function isBalanced(root) {
+  if (root === null) return true;
+
+  const left = findHeight(root.left);
+  const right = findHeight(root.right);
+
+  return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)
+};
