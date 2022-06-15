9d20ffd669e0ee111e302edd59d6f023c5117bc7
Anthony Sim <anthonyysim@gmail.com>
2022-06-15 10:49:40 -0700

    added comments

--- general/graphs/connectedComponentsCount.js
+++ general/graphs/connectedComponentsCount.js
@@ -1,27 +1,32 @@
 function explore(graph, current, visited) {
+  // check if the current node has been visited
   if (visited.has(current.toString())) return false;
 
+  // add current node to visited ex. 0 would be [8, 1, 5]
   visited.add(current.toString());
 
+  // iterate through neighbors ex. 0 neighbors are [8, 1, 5]
   for (let neighbor of graph[current]) {
+    // recursion to visit all connected
     explore(graph, neighbor, visited);
   }
   return true;
 }
 
-
 function countedComponentsCount(graph) {
   const visited = new Set();
   let count = 0;
 
+  // iterate through the "graph" ex. 0, 1, 5, etc.
   for (let node in graph) {
+    // send graph, current node, and new Set to dfs graph search
     if (explore(graph, node, visited)) {
+      // count up when all possibilities have been visited
       count += 1;
     }
   }
   return count;
 }
-
 
 const graph = {
   0: [8, 1, 5],
