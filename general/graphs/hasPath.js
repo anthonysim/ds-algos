09c7e8606311234d40730a213f9331fe5efcbecd
Anthony Sim <anthonyysim@gmail.com>
2023-07-30 20:05:28 -0700

    strictly equals

--- general/graphs/hasPath.js
+++ general/graphs/hasPath.js
@@ -1,5 +1,5 @@
 function hasPath(graph, src, dst) {
-  if (src == dst) return true;
+  if (src === dst) return true;
 
   for (let neighbor of graph[src]) {
     if (hasPath(graph, neighbor, dst)) return true;;
