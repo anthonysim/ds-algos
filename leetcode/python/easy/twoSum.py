a4c599c657915b63748917cc3a18ca3f89db62d9
Anthony Sim <anthonyysim@gmail.com>
2024-05-04 21:15:26 -0700

    two sums python

--- leetcode/python/easy/twoSum.py
+++ leetcode/python/easy/twoSum.py
@@ -1,11 +1,11 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashMap = {}
 
-        for i in range(len(nums)):
-            diff = target - nums[i]
+        for i, num in enumerate(nums):
+            diff = target - num
 
-            if diff in hash:
-                return [hash[diff], i]
-            else:
-                hash[nums[i]] = i
+            if diff in hashMap:
+                return [hashMap[diff], i]
+
+            hashMap[num] = i
