ebf14d37b2f85e8305b849e33ee3ec3674efc3bc
Anthony Sim <anthonyysim@gmail.com>
2024-05-06 20:40:03 -0700

    cleanup

--- leetcode/python/easy/validAnagram.py
+++ leetcode/python/easy/validAnagram.py
@@ -6,6 +6,6 @@ class Solution(object):
         countS, countT = {}, {}
 
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
         return countS == countT
