9165ab581749cbeb998e9f76e8f361e63040372b
Anthony Sim <anthonyysim@gmail.com>
2024-07-06 15:51:37 -0700

    nonoverlap update

--- leetcode/javascript/medium/435-eraseOverlapIntervals.js
+++ leetcode/javascript/medium/435-eraseOverlapIntervals.js
@@ -1,17 +1,20 @@
-function eraseOverlapIntervals(intervals) {
+var eraseOverlapIntervals = function (intervals) {
   intervals.sort((a, b) => a[1] - b[1]);
 
-  let count = 0;
-  let prev = 0;
+  let pos = 0;
+  let total = 0;
 
   for (let i = 1; i < intervals.length; i++) {
-    if (intervals[i][0] < intervals[prev][1]) {
-      count += 1;
+    let prev = intervals[pos];
+    let curr = intervals[i];
+
+    if (prev[1] > curr[0]) {
+      total += 1;
     } else {
-      prev = i;
+      pos = i;
     }
   }
-  return count;
+  return total;
 };
 
 console.log(eraseOverlapIntervals([[1, 2], [2, 3], [3, 4], [1, 3]])); // 1
