b8b2aade16d3918f5afe6eeeace6c557b22e92d2
Anthony Sim <anthonyysim@gmail.com>
2024-07-06 12:32:54 -0700

    uniquePaths update

--- leetcode/javascript/medium/062-uniquePaths.js
+++ leetcode/javascript/medium/062-uniquePaths.js
@@ -1,9 +1,8 @@
-function uniquePaths(m, n, memo = {}) {
-  let key = m + ',' + n;
-
+var uniquePaths = function (m, n, memo = {}) {
+  let key = `${m},${n}`;
   if (key in memo) return memo[key];
-  if (m === 0 || n === 0) return 0;
-  if (m === 1 && n === 1) return 1;
+  if (m <= 0 || n <= 0) return 0;
+  if (m === 1 || n === 1) return 1;
 
   memo[key] = uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo);
   return memo[key];
