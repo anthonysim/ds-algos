1842fe42a7b675226d854da556348f64dea8344e
Anthony Sim <anthonyysim@gmail.com>
2024-07-06 12:25:22 -0700

    update numDecodings

--- leetcode/javascript/medium/091-numDecodings.js
+++ leetcode/javascript/medium/091-numDecodings.js
@@ -1,22 +1,18 @@
-var numDecodings = function (s) {
-  let decoding = [];
-  // generate decoding pattern
-  for (let i = 1; i <= 26; i++) {
-    decoding.push(i.toString());
-  }
-
-  let dp = new Array(s.length + 1).fill(0);
+var numDecodings = function (str) {
+  const decode = new Array(26).fill().map((_, i) => (i + 1).toString());
+  let dp = new Array(str.length + 1).fill(0);
   dp[0] = 1;
 
-  for (let i = 0; i <= s.length; i++) {
-    // iterates through the generated numbers above
-    for (let code of decoding) {
-      if (s.slice(i, i + code.length) === code) {
+  for (let i = 0; i < dp.length; i++) {
+
+    for (let code of decode) {
+
+      if (str.slice(i, i + code.length) === code) {
         dp[i + code.length] += dp[i];
       }
     }
   }
-  return dp[dp.length - 1];
+  return dp.at(-1);
 };
 
 // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
