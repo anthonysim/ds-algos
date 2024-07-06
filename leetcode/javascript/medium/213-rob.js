45bebe78adb6e364273162d75f1a02dd419188f8
Anthony Sim <anthonyysim@gmail.com>
2024-07-06 11:23:41 -0700

    update rob

--- leetcode/javascript/medium/213-rob.js
+++ leetcode/javascript/medium/213-rob.js
@@ -1,23 +1,20 @@
-function helper(arr) {
-  let prev = 0;
-  let curr = 0;
-  let temp;
+function robber(nums) {
+  let rob1 = 0;
+  let rob2 = 0;
 
-  for (let num of arr) {
-    temp = curr;
-    curr = Math.max(num + prev, curr);
-    prev = temp;
+  for (let num of nums) {
+    let temp = Math.max(rob1 + num, rob2);
+    rob1 = rob2;
+    rob2 = temp;
   }
-  return curr;
+  return rob2;
 }
 
-function rob(nums) {
+var rob = function (nums) {
   if (nums.length === 1) return nums[0];
-  let rob1 = nums.slice(0, nums.length - 1);
-  let rob2 = nums.slice(1);
 
-  rob1 = helper(rob1);
-  rob2 = helper(rob2);
+  let first = nums.slice(0, nums.length - 1);
+  let second = nums.slice(1);
 
-  return Math.max(rob1, rob2);
+  return Math.max(robber(first), robber(second));
 };
