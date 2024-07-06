ec7c805f88525c7297b5f52de0befd90ed2608e3
Anthony Sim <anthonyysim@gmail.com>
2024-07-05 20:52:00 -0700

    update lengthOfLIS

--- leetcode/javascript/medium/300-lengthOfLIS.js
+++ leetcode/javascript/medium/300-lengthOfLIS.js
@@ -1,17 +1,17 @@
-function lengthOfLIS(nums) {
-  if (!nums.length) return 0;
-
-  let arr = new Array(nums.length).fill(1);
+var lengthOfLIS = function (nums) {
+  let dp = new Array(nums.length).fill(1);
 
   for (let i = 1; i < nums.length; i++) {
+
     for (let j = 0; j < i; j++) {
+
       if (nums[j] < nums[i]) {
-        arr[i] = Math.max(arr[i], arr[j] + 1);
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
   }
-  return Math.max(...arr);
-}
+  return Math.max(...dp);
+};
 
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])); // 4
 
