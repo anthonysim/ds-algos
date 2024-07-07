4cbefae93bc0dcced394a7f1871a3287f26185b5
Anthony Sim <anthonyysim@gmail.com>
2024-07-07 14:18:12 -0700

    update exist

--- leetcode/javascript/medium/079-exist.js
+++ leetcode/javascript/medium/079-exist.js
@@ -1,38 +1,33 @@
-function dfs(board, i, j, remain) {
-  // if it reached the very end and finds every char, return true;
-  if (remain.length === 0) return true;
+function explore(board, i, j, remain) {
+  if (remain === "") return true;
 
-  // if it reaches outside of the board or no match, return false;
-  if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
+  if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+    return false;
+  }
 
-  // return false if there is no match
   if (board[i][j] !== remain[0]) return false;
 
-  // cross out already visited
-  let char = board[i][j];
-  board[i][j] = '-';
+  let temp = board[i][j];
+  board[i][j] = "-";
 
-  let result = (
-    dfs(board, i + 1, j, remain.substring(1)) // checks down
-    || dfs(board, i - 1, j, remain.substring(1)) // checks  up
-    || dfs(board, i, j + 1, remain.substring(1)) // checks right
-    || dfs(board, i, j - 1, remain.substring(1)) // checks left
-  );
-  // if false, place char back in.
-  board[i][j] = char;
+  let res = (
+    explore(board, i + 1, j, remain.slice(1))
+    || explore(board, i - 1, j, remain.slice(1))
+    || explore(board, i, j + 1, remain.slice(1))
+    || explore(board, i, j - 1, remain.slice(1))
+  )
 
-  return result;
+  board[i][j] = temp;
+  return res;
 }
 
-function exist(board, word) {
-  // if the board or word is empty, return false;
-  if (board.length === 0 || word.length === 0) return false;
+var exist = function (board, word) {
 
-  // potentially iterates through every single item in the matrix.
   for (let i = 0; i < board.length; i++) {
+
     for (let j = 0; j < board[0].length; j++) {
-      // backtracking/recursion to check each char in the matrix.
-      if (dfs(board, i, j, word)) {
+
+      if (explore(board, i, j, word)) {
         return true;
       }
     }
