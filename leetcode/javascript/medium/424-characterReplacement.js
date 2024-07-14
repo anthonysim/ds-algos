9c87e236dd49c9795d009a974857ff50ae449232
Anthony Sim <anthonyysim@gmail.com>
2024-07-14 14:27:53 -0700

    update

--- leetcode/javascript/medium/424-characterReplacement.js
+++ leetcode/javascript/medium/424-characterReplacement.js
@@ -1,19 +1,19 @@
-function characterReplacement(str, k) {
+var characterReplacement = function (str, k) {
   const hash = {};
   let max = 0;
   let start = 0;
 
   for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    if (!(char in hash)) hash[char] = 0;
-    hash[char] += 1;
+    let rightChar = str[i];
+    if (!(rightChar in hash)) hash[rightChar] = 0;
+    hash[rightChar] += 1;
 
-    // in the case that the current window is not valid, shrink the window
     while ((i - start + 1) - Math.max(...Object.values(hash)) > k) {
-      let item = str[start];
-      hash[item] -= 1;
+      let leftChar = str[start];
+      hash[leftChar] -= 1;
       start += 1;
     }
+
     max = Math.max(max, i - start + 1);
   }
   return max;
