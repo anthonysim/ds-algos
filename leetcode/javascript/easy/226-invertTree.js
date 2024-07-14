3d8c12c2aec8df1edccb06ee740fdb87b0dd89c9
Anthony Sim <anthonyysim@gmail.com>
2024-07-14 15:23:12 -0700

    update

--- leetcode/javascript/easy/226-invertTree.js
+++ leetcode/javascript/easy/226-invertTree.js
@@ -1,26 +1,18 @@
 var invertTree = function (root) {
-  if (root === null) {
-    return null;
+  if (!root) return null;
+
+  function dfs(node) {
+    let temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+
+    if (node.left) dfs(node.left);
+    if (node.right) dfs(node.right);
+
+    return root;
   }
-  const queue = [root];
-  const results = [];
-  let temp;
 
-  while (queue.length) {
-    let len = queue.length;
-
-    for (let i = 0; i < len; i++) {
-      const node = queue.shift();
-
-      temp = node.left;
-      node.left = node.right;
-      node.right = temp;
-
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-  }
-  return root;
+  return dfs(root);
 };
 
 // Time Complexity O(V + E)
