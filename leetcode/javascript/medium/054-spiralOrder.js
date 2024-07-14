1a4981b1b1f577a26f597629ab85eebea7c64155
Anthony Sim <anthonyysim@gmail.com>
2024-07-14 13:50:19 -0700

    update

--- leetcode/javascript/medium/054-spiralOrder.js
+++ leetcode/javascript/medium/054-spiralOrder.js
@@ -1,9 +1,5 @@
-function spiralOrder(matrix) {
-  let result = [];
-
-  if (matrix.length == 0) {
-    return result;
-  }
+var spiralOrder = function (matrix) {
+  let res = [];
 
   let rowBegin = 0;
   let rowEnd = matrix.length - 1;
@@ -11,31 +7,30 @@ function spiralOrder(matrix) {
   let colEnd = matrix[0].length - 1;
 
   while (rowBegin <= rowEnd && colBegin <= colEnd) {
-    //right
+
     for (let i = colBegin; i <= colEnd; i++) {
-      result.push(matrix[rowBegin][i])
+      res.push(matrix[rowBegin][i]);
     }
     rowBegin += 1;
-    // far right
+
     for (let i = rowBegin; i <= rowEnd; i++) {
-      result.push(matrix[i][colEnd])
+      res.push(matrix[i][colEnd]);
     }
     colEnd -= 1;
 
     if (!(rowBegin <= rowEnd && colBegin <= colEnd)) break;
 
-    //scan bottom right to left
     for (let i = colEnd; i >= colBegin; i--) {
-      result.push(matrix[rowEnd][i])
+      res.push(matrix[rowEnd][i]);
     }
     rowEnd -= 1;
-    //scan left to top
+
     for (let i = rowEnd; i >= rowBegin; i--) {
-      result.push(matrix[i][colBegin])
+      res.push(matrix[i][colBegin]);
     }
     colBegin += 1;
   }
-  return result;
+  return res;
 };
 
 console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])); // [1, 2, 3, 6, 9, 8, 7, 4, 5]
