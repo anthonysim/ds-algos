120cb85cf97fc1ca999d70ad91b07e4f7dd60113
Anthony Sim <anthonyysim@gmail.com>
2024-07-14 15:44:58 -0700

    update

--- leetcode/javascript/easy/235-lowestCommonAncestor.js
+++ leetcode/javascript/easy/235-lowestCommonAncestor.js
@@ -2,10 +2,10 @@ var lowestCommonAncestor = function (root, p, q) {
   let current = root;
 
   while (current) {
-    if (p.val > current.val && q.val > current.val) {
-      current = current.right;
-    } else if (p.val < current.val && q.val < current.val) {
+    if (p.val < current.val && q.val < current.val) {
       current = current.left;
+    } else if (p.val > current.val && q.val > current.val) {
+      current = current.right;
     } else {
       return current;
     }
