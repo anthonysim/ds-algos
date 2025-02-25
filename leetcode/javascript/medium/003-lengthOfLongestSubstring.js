5e68c5e5e8821d9e87533a065faf90d713536c16
Anthony Sim <anthonyysim@gmail.com>
2025-02-24 16:57:32 -0800

    update

--- leetcode/javascript/medium/003-lengthOfLongestSubstring.js
+++ leetcode/javascript/medium/003-lengthOfLongestSubstring.js
@@ -1,40 +1,19 @@
 var lengthOfLongestSubstring = function (str) {
-  const hash = {};
-  let start = 0;
+  const set = new Set();
   let max = 0;
+  let start = 0;
 
   for (let i = 0; i < str.length; i++) {
     let rightChar = str[i];
 
-    if (!(rightChar in hash)) hash[rightChar] = 0;
-    hash[rightChar] += 1;
-
-    while (hash[rightChar] > 1) {
+    while (set.has(rightChar)) {
       let leftChar = str[start];
+      set.delete(leftChar);
       start += 1;
-
-      if (leftChar in hash) hash[leftChar] -= 1;
-      if (hash[leftChar] === 0) delete hash[leftChar];
     }
+
+    set.add(rightChar);
     max = Math.max(max, i - start + 1);
-  }
-  return max;
-};
-
-/// cleaner version
-var lengthOfLongestSubstring = function (str) {
-  const set = new Set();
-  let left = 0;
-  let max = 0;
-
-  for (let right = 0; right < str.length; right++) {
-    // in the case that the current window is not valid, shrink the window
-    while (set.has(str[right])) {
-      set.delete(str[left]);
-      left += 1;
-    }
-    set.add(str[right]);
-    max = Math.max(max, set.size);
   }
   return max;
 };
