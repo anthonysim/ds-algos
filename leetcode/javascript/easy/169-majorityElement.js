cb036eeead34485e72618d38ed7b8e0725a5cb54
Anthony Sim <anthonyysim@gmail.com>
2025-03-06 21:47:02 -0800

    comments

--- leetcode/javascript/easy/169-majorityElement.js
+++ leetcode/javascript/easy/169-majorityElement.js
@@ -4,8 +4,10 @@ var majorityElement = function (nums) {
   let hightestCount = 0;
 
   for (let num of nums) {
+    // count the number of times each number appears
     hash[num] = hash[num] + 1 || 1;
-
+    // if the count of the current number is greater than the highest count
+    // update the result and the highest count
     if (hash[num] > hightestCount) {
       res = num;
       hightestCount = hash[num];
