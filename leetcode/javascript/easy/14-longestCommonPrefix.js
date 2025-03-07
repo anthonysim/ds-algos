bf2274b8e97559bf17adae98c26d004ea121e5f9
Anthony Sim <anthonyysim@gmail.com>
2025-03-06 20:50:19 -0800

    update

--- leetcode/javascript/easy/14-longestCommonPrefix.js
+++ leetcode/javascript/easy/14-longestCommonPrefix.js
@@ -1,16 +1,17 @@
-function longestCommonPrefix(strs) {
+var longestCommonPrefix = function(strs) {
   let prefix = strs[0];
 
   for (let i = 1; i < strs.length; i++) {
-    let j = 0;
+      let j = 0;
+      let compareStr = strs[i];
 
-    while (j < Math.min(prefix.length, strs[i].length)) {
-      if (prefix[j] !== strs[i][j]) {
-        break;
+      while (j < Math.min(prefix.length, compareStr.length)) {
+          if (prefix[j] !== compareStr[j]) {
+              break;
+          }
+          j += 1;
       }
-      j += 1;
-    }
-    prefix = prefix.slice(0, j);
+      prefix = prefix.slice(0, j);
   }
   return prefix;
-}
+};
