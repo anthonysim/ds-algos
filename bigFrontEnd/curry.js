cca3654c0f81acd8c8236cd0342a8d179efc7f81
Anthony Sim <anthonyysim@gmail.com>
2025-03-21 18:04:04 -0700

    comment

--- bigFrontEnd/curry.js
+++ bigFrontEnd/curry.js
@@ -6,8 +6,6 @@ function curry(fn) {
       return function (...nextArgs) {
         /* if condition above if not satified it will recursively call
         ...args, ...nextArgs is spread operator which will combine the two array into one array
-        args = [1];
-
         */
         return curried(...args, ...nextArgs);
       };
